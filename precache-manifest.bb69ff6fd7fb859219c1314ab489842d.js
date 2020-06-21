b245def3e95d4b949985d92590d6d6e83309c1a2
PiotrNap <piotr.napierala94@gmail.com>
2020-06-21 15:46:58 +0200

    Updates

---
+++ precache-manifest.bb69ff6fd7fb859219c1314ab489842d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3160b82493caa8c47bda20a76271878",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "986a90acafe19f4bbe27",
+    "url": "/todo-app-react/static/css/main.6de9834f.chunk.css"
+  },
+  {
+    "revision": "bc3ab551c937c2d48294",
+    "url": "/todo-app-react/static/js/2.6664fce7.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/todo-app-react/static/js/2.6664fce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "986a90acafe19f4bbe27",
+    "url": "/todo-app-react/static/js/main.7b5120ea.chunk.js"
+  },
+  {
+    "revision": "faffa15f0aa830611b3c",
+    "url": "/todo-app-react/static/js/runtime-main.91458430.js"
+  }
+]);
